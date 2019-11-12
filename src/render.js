093475eb7003ca316ae3e3d43dbe528981d4e310
Tom Vincent <git@tlvince.com>
2019-11-12 23:24:56 +0000

    fix(render): respect the dark side

--- src/render.js
+++ src/render.js
@@ -10,6 +10,20 @@ export default events => {
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width" />
     <title>Eventlite</title>
+    <style>
+      @media (prefers-color-scheme: dark) {
+        body {
+          color: #f9f9fa;
+          background-color: #2a2a2e;
+        }
+        a {
+          color: #45a1ff;
+        }
+        a:hover {
+          color: #0a84ff;
+        }
+      }
+    </style>
   </head>
   <body>
     <h1>Eventlite</h1>
