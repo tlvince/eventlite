ae51f07bbebef905d4c9266f0321817322cbdfb1
Tom Vincent <git@tlvince.com>
2020-01-14 13:41:46 +0000

    chore: remove prelints

--- lint-staged.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  '*.ts': [
-    'eslint --cache --fix',
-    'git add',
-    'yarn test --bail --findRelatedTests',
-  ],
-  '*.{js,jsx,ts,tsx,json,css,md}': ['prettier --write', 'git add'],
-}
